d3a11862dbbfd21cd73e8fed92aa22c245d34571
DanielSupe <daniel.supelanos@unac.edu.co>
2024-05-24 14:28:36 -0500

    Home 1/2

--- src/modules/home/template/TemplateHome.jsx
+++ src/modules/home/template/TemplateHome.jsx
@@ -1,9 +1,211 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+  Button,
+  IconButton,
+  Input,
+  Textarea,
+  Checkbox,
+} from "@material-tailwind/react";
+import { FingerPrintIcon, UsersIcon } from "@heroicons/react/24/solid";
+import { PageTitle, Footer } from "@/widgets/layout";
+import { FeatureCard, TeamCard } from "@/widgets/cards";
+import { featuresData, teamData, contactData } from "@/data";
+import { Navbar } from "@/widgets/layout";
 
-const TemplateHome = () => {
+export function TemplateHome() {
+
+
+   const routes = [
+  {
+    name: "home",
+    path: "/home",
+    element: <TemplateHome />,
+  },
+  {
+    name: "profile",
+    path: "/profile",
+    element: <TemplateHome />,
+  },
+  {
+    name: "Sign In",
+    path: "/sign-in",
+    element: <TemplateHome />,
+  },
+  {
+    name: "Sign Up",
+    path: "/sign-up",
+    element: <TemplateHome />,
+  },
+];
+
   return (
-    <div className=' text-amber-400'>Hackton</div>
-  )
+    <>
+      <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
+      <div className="container absolute top-0 z-10  p-4">
+          <Navbar routes={routes} />
+        </div>
+        <div className="absolute top-0 h-full w-full bg-[url('/Images/Home/image_1.jpg')] bg-cover bg-center" />
+        <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
+
+
+        <div className="max-w-8xl container relative mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-6 font-black"
+              >
+                Your story starts with us.
+              </Typography>
+              <Typography variant="lead" color="white" className="opacity-80">
+                This is a simple example of a Landing Page you can build using
+                Material Tailwind. It features multiple components based on the
+                Tailwind CSS and Material Design by Google.
+              </Typography>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+      <section className="-mt-32 bg-white px-4 pb-20 pt-4">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 ">
+            {featuresData.map(({ color, title, icon, description }) => (
+              <FeatureCard
+                key={title}
+                color={color}
+                title={title}
+                icon={React.createElement(icon, {
+                  className: "w-5 h-5 text-black",
+                })}
+                description={description}
+              />
+            ))}
+          </div>
+          <div className="mt-32 flex flex-wrap items-center">
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-blue-gray-900 p-2 text-center shadow-lg">
+                <FingerPrintIcon className="h-8 w-8 text-black " />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                Working with us is a pleasure
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+                Dont let your uses guess by attaching tooltips and popoves to
+                any element. Just make sure you enable them first via
+                JavaScript.
+                <br />
+                <br />
+                The kit comes with three pre-built pages to help you get started
+                faster. You can change the text and images and youre good to
+                go. Just make sure you enable them first via JavaScript.
+              </Typography>
+              <Button className=" bg-black text-white px-4 py-2" variant="filled">read more</Button>
+            </div>
+
+            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg border shadow-gray-500/10 rounded-lg box-border p-4 Working with us is a pleasure">
+                <CardHeader floated={false} className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.png"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography variant="small" color="blue-gray" className="font-normal">Enterprise</Typography>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 mt-2 font-bold"
+                  >
+                    Top Notch Services
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens.
+                  </Typography>
+                </CardBody>
+              </Card>
+
+
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="px-4 pt-20 pb-48">
+        <div className="container mx-auto">
+          <PageTitle section="Our Team" heading="Here are our heroes">
+            According to the National Oceanic and Atmospheric Administration,
+            Ted, Scambos, NSIDClead scentist, puts the potentially record
+            maximum.
+          </PageTitle>
+          <div className="mt-24 grid grid-cols-1 gap-12 gap-x-24 md:grid-cols-2 xl:grid-cols-4">
+            {teamData.map(({ img, name, position, socials }) => (
+              <TeamCard
+                key={name}
+                img={img}
+                name={name}
+                position={position}
+                socials={
+                  <div className="flex items-center gap-2">
+                    {socials.map(({ color, name }) => (
+                      <IconButton key={name} color={color} variant="text">
+                        <i className={`fa-brands text-xl fa-${name}`} />
+                      </IconButton>
+                    ))}
+                  </div>
+                }
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="relative bg-white py-24 px-4">
+        <div className="container mx-auto">
+          <PageTitle section="Co-Working" heading="Build something">
+            Put the potentially record low maximum sea ice extent tihs year down
+            to low ice. According to the National Oceanic and Atmospheric
+            Administration, Ted, Scambos.
+          </PageTitle>
+          <div className="mx-auto mt-20 mb-48 grid max-w-5xl grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+            {contactData.map(({ title, icon, description }) => (
+              <Card
+                key={title}
+                color="transparent"
+                shadow={false}
+                className="text-center text-blue-gray-900"
+              >
+                <div className="mx-auto mb-6 grid h-14 w-14 place-items-center rounded-full bg-blue-gray-900 shadow-lg shadow-gray-500/20">
+                  {React.createElement(icon, {
+                    className: "w-5 h-5 text-black",
+                  })}
+                </div>
+                <Typography variant="h5" color="blue-gray" className="mb-2">
+                  {title}
+                </Typography>
+                <Typography className="font-normal text-blue-gray-500">
+                  {description}
+                </Typography>
+              </Card>
+            ))}
+          </div>
+          
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default TemplateHome
+export default TemplateHome;
