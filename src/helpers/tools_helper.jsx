606fb107bba48cf284abd3c1c54d2456bea5b03d
DanielSupe <daniel.supelanos@unac.edu.co>
2024-05-30 00:58:26 -0500

    dinamic roles

--- src/helpers/tools_helper.jsx
+++ src/helpers/tools_helper.jsx
@@ -2,3 +2,17 @@ export const obtenerUser = ()=>{
     const user = JSON.parse(localStorage.getItem("USER"));
     return user;
 }
+
+export const obtenerUrlImageRol = ()=>{
+    const {rol} = JSON.parse(localStorage.getItem("USER"));
+    switch(rol){
+        case "mago":
+            return "/Images/HomeHeroe/magoF.png"
+        case "guerrero":
+            return "/Images/HomeHeroe/guerreroF.png"
+        case "caballero":
+            return "/Images/HomeHeroe/caballeroF.png"
+        case "arquero":
+            return "/Images/HomeHeroe/arqueroF.png"
+    }
+}   
